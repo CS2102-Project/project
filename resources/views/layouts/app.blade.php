e65282e340ba9f800f7e669924bdeeb9b9f462b3
DongShaocong <shaocong.dong@yahoo.com>
2017-09-28 12:11:20 +0800

    Navbar for logged_in user, authentication draft done

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -42,7 +42,7 @@
                     <!-- Right Side Of Navbar -->
                     <ul class="nav navbar-nav navbar-right">
                         <!-- Authentication Links -->
-                        @if (!Auth::check())
+                        @guest
                             <li><a href="{{ route('login') }}">Login</a></li>
                             <li><a href="{{ route('register') }}">Register</a></li>
                         @else
