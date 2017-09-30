9c0c875b74b3ce3508e1bd9d3afba892be8e841e
DongShaocong <qq423785549@gmail.com>
2017-09-30 17:12:19 +0800

    Add schemas final version and data insertions

---
+++ resources/SQL_database_management/Items.sql
@@ -0,0 +1,59 @@
+INSERT INTO items (name, owner, description, available)
+VALUES ('Groot Figure Toy', 'huangran1991@yahoo.com', 'Marvel Guardians of the Galaxy
+ Dancing Groot Bobble Figure Toy Doll Funko POP
+ This is a Chinese version of the POP. ', 'TRUE');
+INSERT INTO items (name, owner, description, available)
+VALUES ('Bicycle', 'jjtabc@gmail.com', 'Classic Moulton Mini Deluxe Bicycle', 'TRUE');
+INSERT INTO items (name, owner, description, available)
+VALUES ('Mountain Bike', 'mmb@gmail.com', 'Ancheer 14"Mini Electric Power Bicycle
+ 26" Foldable Electric Mountain Bike', 'TRUE');
+INSERT INTO items (name, owner, description, available)
+VALUES ('Book on the road', 'ly@gmail.com', 'JACK KEROUAC 1st edition of his MOST
+ FAMOUS book ON THE ROAD', 'TRUE');
+INSERT INTO items (name, owner, description, available)
+VALUES ('The Anatomy Coloring Book', 'dy123@gmail.com', 'The Anatomy Coloring Book 4th
+ Edition by Wynn Kapit (English) Paperback Book', 'TRUE');
+INSERT INTO items (name, owner, description, available)
+VALUES ('Adaptor', 'zhangkangqiao@gmail.com', 'Macbook adaptor in good condition', 'TRUE');
+INSERT INTO items (name, owner, description, available)
+VALUES ('Hand Druck', 'tyabc@gmail.com', 'hmk111ua4 aluminum hand truck, horizontal
+ loop handle, 18" x 7-1/2" aluminum', 'TRUE');
+INSERT INTO items (name, owner, description, available)
+VALUES ('Play Station 4', 'fengyangzuibang@gmail.com', 'Sony PlayStation 4 PS4 500GB Original
+Matte Black CUH-1215A ONE CONSOLE', 'TRUE');
+INSERT INTO items (name, owner, description, available)
+VALUES ('Xbox One', 'longxinl@gmail.com', 'MICROSOFT XBOX ONE S 500GB BATTLEFIELD 1
+ CONSOLE BUNDLE ZQ9-0028 - WHITE', 'TRUE');
+INSERT INTO items (name, owner, description, available)
+VALUES ('Step Ladder', 'zkchaoshuai@gmail.com', 'Louisville Ladder AS3002 300-Pound Duty
+ Rating Aluminum Stepladder 2-Foot', 'TRUE');
+INSERT INTO items (name, owner, description, available)
+VALUES ('VR box','huangran1991@yahoo.com','Vr Box Virtual Reality Movies Games 3d Glasses
+ For 4.7-6.0 Smart Phone','TRUE');
+INSERT INTO items (name, owner, description, available)
+VALUES ('Funko Pop','jjtabc@gmail.com','Funko Pop Disney Alice In Wonderland: Mad Hatter Vinyl
+ Collectible Action Figure','TRUE');
+INSERT INTO items (name, owner, description, available)
+VALUES ('Step Ladder','mmb@gmail.com','Folding Aluminium Ladder 3 Non Slip Ribbed Steps 3.3kg',
+ 'TRUE');
+INSERT INTO items (name, owner, description, available)
+VALUES ('Sketch Box','ly@gmail.com','French Easel Wooden Sketch Box Portable Folding Durable
+ Artist Painters Tripod','TRUE');
+INSERT INTO items (name, owner, description, available)
+VALUES ('Tool Box','dy123@gmail.com','3 PC TOOL BOX INSIDE DRAWER ORGANIZER DIVIDER FOR
+ KITCHEN DESK TOOL ROLLAWAY','TRUE');
+INSERT INTO items (name, owner, description, available)
+VALUES ('Computer Repair Tool','zhangkangqiao@gmail.com','Computer Repair Tool Kit Precision
+ For Laptop Electronics PC Repair 76 in 1 Set','TRUE');
+INSERT INTO items (name, owner, description, available)
+VALUES ('LEGO','tyabc@gmail.com','LEGO Elsa Magical Ice Palace Frozen Disney Princess
+ Set 41148','TRUE');
+INSERT INTO items (name, owner, description, available)
+VALUES ('Book','fengyangzuibang@gmail.com','Star Wars Art: Concept by Lucasfilm Ltd
+ (English) Hardcover Book','TRUE');
+INSERT INTO items (name, owner, description, available)
+VALUES ('Textbook','longxinl@gmail.com','LINUX ALL-IN-ONE FOR DUMMIES - DULANEY, EMMETT -
+ NEW PAPERBACK BOOK','TRUE');
+INSERT INTO items (name, owner, description, available)
+VALUES ('Screwdriver','zkchaoshuai@gmail.com','Makita XSF03Z 18V Lithium-Ion Brushless
+ Cordless Drywall Screwdriver, Bare Tool','TRUE');
---
+++ resources/SQL_database_management/bids.sql
@@ -0,0 +1,200 @@
+INSERT INTO bids (bidder, post, points, status)
+VALUES ('mingzhouaaa@gmail.com', '1', '1', 'FAILURE');
+INSERT INTO bids (bidder, post, points, status)
+VALUES ('wulei@gmail.com', '1', '2', 'FAILURE');
+INSERT INTO bids (bidder, post, points, status)
+VALUES ('wuzhaolihai@gmail.com', '1', '3', 'FAILURE');
+INSERT INTO bids (bidder, post, points, status)
+VALUES ('zhangzexian@gmail.com', '1', '4', 'FAILURE');
+INSERT INTO bids (bidder, post, points, status)
+VALUES ('yongsun@gmail.com', '1', '5', 'FAILURE');
+INSERT INTO bids (bidder, post, points, status)
+VALUES ('haixia@gmail.com', '1', '6', 'FAILURE');
+INSERT INTO bids (bidder, post, points, status)
+VALUES ('tugaoshuaiqi@gmail.com', '1', '7', 'FAILURE');
+INSERT INTO bids (bidder, post, points, status)
+VALUES ('yubailihai@gmail.com', '1', '8', 'FAILURE');
+INSERT INTO bids (bidder, post, points, status)
+VALUES ('xiexin2011@gmail.com', '1', '9', 'FAILURE');
+INSERT INTO bids (bidder, post, points, status)
+VALUES ('123456789@qq.com', '1', '10', 'SUCCESS');
+INSERT INTO bids (bidder, post, points, status)
+VALUES ('123456789@qq.com', '2', '1', 'FAILURE');
+INSERT INTO bids (bidder, post, points, status)
+VALUES ('xiexin2011@gmail.com', '2', '2', 'FAILURE');
+INSERT INTO bids (bidder, post, points, status)
+VALUES ('yubailihai@gmail.com', '2', '3', 'FAILURE');
+INSERT INTO bids (bidder, post, points, status)
+VALUES ('tugaoshuaiqi@gmail.com', '2', '4', 'FAILURE');
+INSERT INTO bids (bidder, post, points, status)
+VALUES ('haixia@gmail.com', '2', '5', 'FAILURE');
+INSERT INTO bids (bidder, post, points, status)
+VALUES ('yongsun@gmail.com', '2', '6', 'FAILURE');
+INSERT INTO bids (bidder, post, points, status)
+VALUES ('zhangzexian@gmail.com', '2', '7', 'FAILURE');
+INSERT INTO bids (bidder, post, points, status)
+VALUES ('wuzhaolihai@gmail.com', '2', '8', 'FAILURE');
+INSERT INTO bids (bidder, post, points, status)
+VALUES ('wulei@gmail.com', '2', '9', 'FAILURE');
+INSERT INTO bids (bidder, post, points, status)
+VALUES ('mingzhouaaa@gmail.com', '2', '10', 'SUCCESS');
+INSERT INTO bids (bidder, post, points, status)
+VALUES ('yongsun@gmail.com', '3', '1', 'FAILURE');
+INSERT INTO bids (bidder, post, points, status)
+VALUES ('wuzhaolihai@gmail.com', '3', '2', 'FAILURE');
+INSERT INTO bids (bidder, post, points, status)
+VALUES ('haixia@gmail.com', '3', '3', 'FAILURE');
+INSERT INTO bids (bidder, post, points, status)
+VALUES ('mingzhouaaa@gmail.com', '3', '4', 'FAILURE');
+INSERT INTO bids (bidder, post, points, status)
+VALUES ('tugaoshuaiqi@gmail.com', '3', '5', 'FAILURE');
+INSERT INTO bids (bidder, post, points, status)
+VALUES ('zhangzexian@gmail.com', '3', '6', 'FAILURE');
+INSERT INTO bids (bidder, post, points, status)
+VALUES ('yubailihai@gmail.com', '3', '7', 'FAILURE');
+INSERT INTO bids (bidder, post, points, status)
+VALUES ('wulei@gmail.com', '3', '8', 'FAILURE');
+INSERT INTO bids (bidder, post, points, status)
+VALUES ('xiexin2011@gmail.com', '3', '9', 'FAILURE');
+INSERT INTO bids (bidder, post, points, status)
+VALUES ('123456789@qq.com', '3', '10', 'SUCCESS');
+INSERT INTO bids (bidder, post, points, status)
+VALUES ('tugaoshuaiqi@gmail.com', '4', '1', 'FAILURE');
+INSERT INTO bids (bidder, post, points, status)
+VALUES ('wulei@gmail.com', '4', '2', 'FAILURE');
+INSERT INTO bids (bidder, post, points, status)
+VALUES ('haixia@gmail.com', '4', '3', 'FAILURE');
+INSERT INTO bids (bidder, post, points, status)
+VALUES ('123456789@qq.com', '4', '4', 'FAILURE');
+INSERT INTO bids (bidder, post, points, status)
+VALUES ('zhangzexian@gmail.com', '4', '5', 'FAILURE');
+INSERT INTO bids (bidder, post, points, status)
+VALUES ('mingzhouaaa@gmail.com', '4', '6', 'FAILURE');
+INSERT INTO bids (bidder, post, points, status)
+VALUES ('xiexin2011@gmail.com', '4', '7', 'FAILURE');
+INSERT INTO bids (bidder, post, points, status)
+VALUES ('wuzhaolihai@gmail.com', '4', '8', 'FAILURE');
+INSERT INTO bids (bidder, post, points, status)
+VALUES ('yubailihai@gmail.com', '4', '9', 'FAILURE');
+INSERT INTO bids (bidder, post, points, status)
+VALUES ('yongsun@gmail.com', '4', '10', 'SUCCESS');
+INSERT INTO bids (bidder, post, points, status)
+VALUES ('yongsun@gmail.com', '5', '1', 'FAILURE');
+INSERT INTO bids (bidder, post, points, status)
+VALUES ('haixia@gmail.com', '5', '2', 'FAILURE');
+INSERT INTO bids (bidder, post, points, status)
+VALUES ('wulei@gmail.com', '5', '3', 'FAILURE');
+INSERT INTO bids (bidder, post, points, status)
+VALUES ('tugaoshuaiqi@gmail.com', '5', '4', 'FAILURE');
+INSERT INTO bids (bidder, post, points, status)
+VALUES ('xiexin2011@gmail.com', '5', '5', 'FAILURE');
+INSERT INTO bids (bidder, post, points, status)
+VALUES ('mingzhouaaa@gmail.com', '5', '6', 'FAILURE');
+INSERT INTO bids (bidder, post, points, status)
+VALUES ('zhangzexian@gmail.com', '5', '7', 'FAILURE');
+INSERT INTO bids (bidder, post, points, status)
+VALUES ('wuzhaolihai@gmail.com', '5', '8', 'FAILURE');
+INSERT INTO bids (bidder, post, points, status)
+VALUES ('123456789@qq.com', '5', '9', 'FAILURE');
+INSERT INTO bids (bidder, post, points, status)
+VALUES ('yubailihai@gmail.com', '5', '10', 'SUCCESS');
+INSERT INTO bids (bidder, post, points, status)
+VALUES ('haixia@gmail.com', '6', '1', 'FAILURE');
+INSERT INTO bids (bidder, post, points, status)
+VALUES ('xiexin2011@gmail.com', '6', '2', 'FAILURE');
+INSERT INTO bids (bidder, post, points, status)
+VALUES ('yubailihai@gmail.com', '6', '3', 'FAILURE');
+INSERT INTO bids (bidder, post, points, status)
+VALUES ('yongsun@gmail.com', '6', '4', 'FAILURE');
+INSERT INTO bids (bidder, post, points, status)
+VALUES ('tugaoshuaiqi@gmail.com', '6', '5', 'FAILURE');
+INSERT INTO bids (bidder, post, points, status)
+VALUES ('mingzhouaaa@gmail.com', '6', '6', 'FAILURE');
+INSERT INTO bids (bidder, post, points, status)
+VALUES ('wuzhaolihai@gmail.com', '6', '7', 'FAILURE');
+INSERT INTO bids (bidder, post, points, status)
+VALUES ('zhangzexian@gmail.com', '6', '8', 'FAILURE');
+INSERT INTO bids (bidder, post, points, status)
+VALUES ('wulei@gmail.com', '6', '9', 'FAILURE');
+INSERT INTO bids (bidder, post, points, status)
+VALUES ('123456789@qq.com', '6', '10', 'SUCCESS');
+INSERT INTO bids (bidder, post, points, status)
+VALUES ('123456789@qq.com', '7', '1', 'FAILURE');
+INSERT INTO bids (bidder, post, points, status)
+VALUES ('xiexin2011@gmail.com', '7', '2', 'FAILURE');
+INSERT INTO bids (bidder, post, points, status)
+VALUES ('zhangzexian@gmail.com', '7', '3', 'FAILURE');
+INSERT INTO bids (bidder, post, points, status)
+VALUES ('mingzhouaaa@gmail.com', '7', '4', 'FAILURE');
+INSERT INTO bids (bidder, post, points, status)
+VALUES ('yubailihai@gmail.com', '7', '5', 'FAILURE');
+INSERT INTO bids (bidder, post, points, status)
+VALUES ('yongsun@gmail.com', '7', '6', 'FAILURE');
+INSERT INTO bids (bidder, post, points, status)
+VALUES ('tugaoshuaiqi@gmail.com', '7', '7', 'FAILURE');
+INSERT INTO bids (bidder, post, points, status)
+VALUES ('haixia@gmail.com', '7', '8', 'FAILURE');
+INSERT INTO bids (bidder, post, points, status)
+VALUES ('wulei@gmail.com', '7', '9', 'FAILURE');
+INSERT INTO bids (bidder, post, points, status)
+VALUES ('wuzhaolihai@gmail.com', '7', '10', 'SUCCESS');
+INSERT INTO bids (bidder, post, points, status)
+VALUES ('xiexin2011@gmail.com', '8', '1', 'FAILURE');
+INSERT INTO bids (bidder, post, points, status)
+VALUES ('wuzhaolihai@gmail.com', '8', '2', 'FAILURE');
+INSERT INTO bids (bidder, post, points, status)
+VALUES ('yubailihai@gmail.com', '8', '3', 'FAILURE');
+INSERT INTO bids (bidder, post, points, status)
+VALUES ('zhangzexian@gmail.com', '8', '4', 'FAILURE');
+INSERT INTO bids (bidder, post, points, status)
+VALUES ('yongsun@gmail.com', '8', '5', 'FAILURE');
+INSERT INTO bids (bidder, post, points, status)
+VALUES ('tugaoshuaiqi@gmail.com', '8', '6', 'FAILURE');
+INSERT INTO bids (bidder, post, points, status)
+VALUES ('123456789@qq.com', '8', '7', 'FAILURE');
+INSERT INTO bids (bidder, post, points, status)
+VALUES ('wulei@gmail.com', '8', '8', 'FAILURE');
+INSERT INTO bids (bidder, post, points, status)
+VALUES ('haixia@gmail.com', '8', '9', 'FAILURE');
+INSERT INTO bids (bidder, post, points, status)
+VALUES ('mingzhouaaa@gmail.com', '8', '10', 'SUCCESS');
+INSERT INTO bids (bidder, post, points, status)
+VALUES ('123456789@qq.com', '9', '1', 'FAILURE');
+INSERT INTO bids (bidder, post, points, status)
+VALUES ('yubailihai@gmail.com', '9', '2', 'FAILURE');
+INSERT INTO bids (bidder, post, points, status)
+VALUES ('zhangzexian@gmail.com', '9', '3', 'FAILURE');
+INSERT INTO bids (bidder, post, points, status)
+VALUES ('haixia@gmail.com', '9', '4', 'FAILURE');
+INSERT INTO bids (bidder, post, points, status)
+VALUES ('mingzhouaaa@gmail.com', '9', '5', 'FAILURE');
+INSERT INTO bids (bidder, post, points, status)
+VALUES ('tugaoshuaiqi@gmail.com', '9', '6', 'FAILURE');
+INSERT INTO bids (bidder, post, points, status)
+VALUES ('xiexin2011@gmail.com', '9', '7', 'FAILURE');
+INSERT INTO bids (bidder, post, points, status)
+VALUES ('wuzhaolihai@gmail.com', '9', '8', 'FAILURE');
+INSERT INTO bids (bidder, post, points, status)
+VALUES ('wulei@gmail.com', '9', '9', 'FAILURE');
+INSERT INTO bids (bidder, post, points, status)
+VALUES ('yongsun@gmail.com', '9', '10', 'SUCCESS');
+INSERT INTO bids (bidder, post, points, status)
+VALUES ('yongsun@gmail.com', '10', '1', 'FAILURE');
+INSERT INTO bids (bidder, post, points, status)
+VALUES ('haixia@gmail.com', '10', '2', 'FAILURE');
+INSERT INTO bids (bidder, post, points, status)
+VALUES ('123456789@qq.com', '10', '3', 'FAILURE');
+INSERT INTO bids (bidder, post, points, status)
+VALUES ('xiexin2011@gmail.com', '10', '4', 'FAILURE');
+INSERT INTO bids (bidder, post, points, status)
+VALUES ('yubailihai@gmail.com', '10', '5', 'FAILURE');
+INSERT INTO bids (bidder, post, points, status)
+VALUES ('wulei@gmail.com', '10', '6', 'FAILURE');
+INSERT INTO bids (bidder, post, points, status)
+VALUES ('zhangzexian@gmail.com', '10', '7', 'FAILURE');
+INSERT INTO bids (bidder, post, points, status)
+VALUES ('mingzhouaaa@gmail.com', '10', '8', 'FAILURE');
+INSERT INTO bids (bidder, post, points, status)
+VALUES ('wuzhaolihai@gmail.com', '10', '9', 'FAILURE');
+INSERT INTO bids (bidder, post, points, status)
+VALUES ('tugaoshuaiqi@gmail.com', '10', '10', 'SUCCESS');
