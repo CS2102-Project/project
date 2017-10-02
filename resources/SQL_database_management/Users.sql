a4761151b6573d90715aadbd355af728296b81df
LiuYulin0629 <yulinliu668@gmail.com>
2017-10-02 15:25:41 +0800

    Update Users.sql

--- resources/SQL_database_management/Users.sql
+++ resources/SQL_database_management/Users.sql
@@ -1,40 +1,40 @@
 INSERT INTO users (email, username, password, mobile, address, credit_rating)
-VALUES ('huangran1991@yahoo.com', 'HUANG RAN', 'abcdefg','65427843','1 Fusionopolis Walk, #11-04 South Tower, Solaris, Singapore 138628', '5');
+VALUES ('huangran1991@yahoo.com', 'HUANG RAN', 'abcdefg','65427843','1 Fusionopolis Walk, #11-04 South Tower, Solaris, Singapore 138628', 5);
 INSERT INTO users (email, username, password, mobile, address, credit_rating)
-VALUES ('jjtabc@gmail.com','Jaime J Trevino','812494efg','61249479','1922 Heliport Loop, Bloomington, Indiana, US', '5');
+VALUES ('jjtabc@gmail.com','Jaime J Trevino','812494efg','61249479','1922 Heliport Loop, Bloomington, Indiana, US', 5);
 INSERT INTO users (email, username, password, mobile, address, credit_rating)
-VALUES ('mmb@gmail.com','Mark M Brown','3283abc','68163283','1486 Wildrose Lane, LANESBOROUGH, Massachusetts, US', '5');
+VALUES ('mmb@gmail.com','Mark M Brown','3283abc','68163283','1486 Wildrose Lane, LANESBOROUGH, Massachusetts, US', 5);
 INSERT INTO users (email, username, password, mobile, address, credit_rating)
-VALUES ('ly@gmail.com','Lim Yao','65674fxd','65674453','171 tras street 04-171a union building, 079025, Singapore', '5');
+VALUES ('ly@gmail.com','Lim Yao','65674fxd','65674453','171 tras street 04-171a union building, 079025, Singapore', 5);
 INSERT INTO users (email, username, password, mobile, address, credit_rating)
-VALUES ('dy123@gmail.com','De Ye','b0ZV1Ej','64969000','1 Fusionopolis Walk, #11-04 South Tower, Solaris, Singapore 138628', '5');
+VALUES ('dy123@gmail.com','De Ye','b0ZV1Ej','64969000','1 Fusionopolis Walk, #11-04 South Tower, Solaris, Singapore 138628', 5);
 INSERT INTO users (email, username, password, mobile, address, credit_rating)
-VALUES ('zhangkangqiao@gmail.com','ZhenKang Qiao','9Pe2NNK','6931234','20 Benoi Crescent Jurong, Singapore 629983', '5');
+VALUES ('zhangkangqiao@gmail.com','ZhenKang Qiao','9Pe2NNK','6931234','20 Benoi Crescent Jurong, Singapore 629983', 5);
 INSERT INTO users (email, username, password, mobile, address, credit_rating)
-VALUES ('tyabc@gmail.com','Tu Yu','7&8*abc123','65432136','6b boon tiong road 10-55, 165006, Singapore', '5');
+VALUES ('tyabc@gmail.com','Tu Yu','7&8*abc123','65432136','6b boon tiong road 10-55, 165006, Singapore', 5);
 INSERT INTO users (email, username, password, mobile, address, credit_rating)
-VALUES ('fengyangzuibang@gmail.com','FengYang','12*e10928','67839124','108 Gerald Drive #04-38, 799035, Singapore', '5');
+VALUES ('fengyangzuibang@gmail.com','FengYang','12*e10928','67839124','108 Gerald Drive #04-38, 799035, Singapore', 5);
 INSERT INTO users (email, username, password, mobile, address, credit_rating)
-VALUES ('longxinl@gmail.com','Lingxin Long','7^2409*&^','69831321','60 Admiralty Road West #02-01 PROJECT OFFICE NO 8, 759947, Singapore', '5');
+VALUES ('longxinl@gmail.com','Lingxin Long','7^2409*&^','69831321','60 Admiralty Road West #02-01 PROJECT OFFICE NO 8, 759947, Singapore', 5);
 INSERT INTO users (email, username, password, mobile, address, credit_rating)
-VALUES ('zkchaoshuai@gmail.com','Zhong Kong','1(731^&543','69821324','48, Hillview Ter #04-01, Hillview Bldg Singapore 669269', '5');
+VALUES ('zkchaoshuai@gmail.com','Zhong Kong','1(731^&543','69821324','48, Hillview Ter #04-01, Hillview Bldg Singapore 669269', 5);
 INSERT INTO users (email, username, password, mobile, address, credit_rating)
-VALUES ('mingzhouaaa@gmail.com','Ming Zhou','7*63*9d','63134924','1 Bedok South Road, 469267, Singapore', '5');
+VALUES ('mingzhouaaa@gmail.com','Ming Zhou','7*63*9d','63134924','1 Bedok South Road, 469267, Singapore', 5);
 INSERT INTO users (email, username, password, mobile, address, credit_rating)
-VALUES ('wulei@gmail.com','Wu Lei','&(32n3edds','68310245','45 Kaki Bukit Place EUNOS TECHPARK, 416223, Singapore', '5');
+VALUES ('wulei@gmail.com','Wu Lei','&(32n3edds','68310245','45 Kaki Bukit Place EUNOS TECHPARK, 416223, Singapore', 5);
 INSERT INTO users (email, username, password, mobile, address, credit_rating)
-VALUES ('wuzhaolihai@gmail.com','Wu Zhao','adicoo3*^','63812232','238 Thomson Road #13-07 NOVENA SQUARE - TOWER A, 307684, Singapore', '5');
+VALUES ('wuzhaolihai@gmail.com','Wu Zhao','adicoo3*^','63812232','238 Thomson Road #13-07 NOVENA SQUARE - TOWER A, 307684, Singapore', 5);
 INSERT INTO users (email, username, password, mobile, address, credit_rating)
-VALUES ('zhangzexian@gmail.com','Zhang Zexian','nfksqowe','63782912','80, Genting Lane #09-07, Genting Blk, Ruby Ind Complex Singapore 349565', '5');
+VALUES ('zhangzexian@gmail.com','Zhang Zexian','nfksqowe','63782912','80, Genting Lane #09-07, Genting Blk, Ruby Ind Complex Singapore 349565', 5);
 INSERT INTO users (email, username, password, mobile, address, credit_rating)
-VALUES ('yongsun@gmail.com','Yong Sun','f10*&@9e','63910242','52 Khasmahal, Jamshedpur, Jharkhand 831002, Singapore', '5');
+VALUES ('yongsun@gmail.com','Yong Sun','f10*&@9e','63910242','52 Khasmahal, Jamshedpur, Jharkhand 831002, Singapore', 5);
 INSERT INTO users (email, username, password, mobile, address, credit_rating)
-VALUES ('haixia@gmail.com','Hai Xia','381hd8s&*^','64328910','507 Yishun Industrial Parka, 768734, Singapore', '5');
+VALUES ('haixia@gmail.com','Hai Xia','381hd8s&*^','64328910','507 Yishun Industrial Parka, 768734, Singapore', 5);
 INSERT INTO users (email, username, password, mobile, address, credit_rating)
-VALUES ('tugaoshuaiqi@gmail.com','Tu Gao','29ud&*(@','63718903','63 market street 01-01 opposite republic plaza, 048942, Singapore', '5');
+VALUES ('tugaoshuaiqi@gmail.com','Tu Gao','29ud&*(@','63718903','63 market street 01-01 opposite republic plaza, 048942, Singapore', 5);
 INSERT INTO users (email, username, password, mobile, address, credit_rating)
-VALUES ('yubailihai@gmail.com','Yu Bai','&(h3ejdkl','63901437','9 Temasek Boulevard #38-01 SUNTEC TOWER 2, 038989, Singapore', '5');
+VALUES ('yubailihai@gmail.com','Yu Bai','&(h3ejdkl','63901437','9 Temasek Boulevard #38-01 SUNTEC TOWER 2, 038989, Singapore', 5);
 INSERT INTO users (email, username, password, mobile, address, credit_rating)
-VALUES ('xiexin2011@gmail.com', 'XIE XIN', '987654321','64213445','18 Tannery Lane 289674, Singapore', '5');
+VALUES ('xiexin2011@gmail.com', 'XIE XIN', '987654321','64213445','18 Tannery Lane 289674, Singapore', 5);
 INSERT INTO users (email, username, password, mobile, address, credit_rating)
-VALUES ('123456789@qq.com', 'happy', '123456789', '63741902', 'Blk 2 Kaki Bukit Ave 1, 03-06/08 Kaki Bukit Industrial Estate, 417938, Singapore', '5');
+VALUES ('123456789@qq.com', 'happy', '123456789', '63741902', 'Blk 2 Kaki Bukit Ave 1, 03-06/08 Kaki Bukit Industrial Estate, 417938, Singapore', 5);
