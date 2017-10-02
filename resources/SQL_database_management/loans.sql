24786233388541e1e700259bdcbd4e58270be8cc
LiuYulin0629 <yulinliu668@gmail.com>
2017-10-02 15:04:39 +0800

    Update loans.sql

--- resources/SQL_database_management/loans.sql
+++ resources/SQL_database_management/loans.sql
@@ -1,20 +1,20 @@
 INSERT INTO loans (bid, post, status)
-VALUES ('10', '1', 'RETURNED');
+VALUES (10, 1, 'RETURNED');
 INSERT INTO loans (bid, post, status)
-VALUES ('20', '2', 'RETURNED');
+VALUES (20, 2, 'RETURNED');
 INSERT INTO loans (bid, post, status)
-VALUES ('30', '3', 'RETURNED');
+VALUES (30, 3, 'RETURNED');
 INSERT INTO loans (bid, post, status)
-VALUES ('40', '4', 'RETURNED');
+VALUES (40, 4, 'RETURNED');
 INSERT INTO loans (bid, post, status)
-VALUES ('50', '5', 'RETURNED');
+VALUES (50, 5, 'RETURNED');
 INSERT INTO loans (bid, post, status)
-VALUES ('60', '6', 'RETURNED');
+VALUES (60, 6, 'RETURNED');
 INSERT INTO loans (bid, post, status)
-VALUES ('70', '7', 'RETURNED');
+VALUES (70, 7, 'RETURNED');
 INSERT INTO loans (bid, post, status)
-VALUES ('80', '8', 'RETURNED');
+VALUES (80, 8, 'RETURNED');
 INSERT INTO loans (bid, post, status)
-VALUES ('90', '9', 'RETURNED');
+VALUES (90, 9, 'RETURNED');
 INSERT INTO loans (bid, post, status)
-VALUES ('100', '10', 'RETURNED');
+VALUES (100, 10, 'RETURNED');
