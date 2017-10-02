6e2daeb3e9fba408bed77ed98a978db183353f59
LiuYulin0629 <yulinliu668@gmail.com>
2017-10-02 15:02:18 +0800

    Update posts.sql

--- resources/SQL_database_management/posts.sql
+++ resources/SQL_database_management/posts.sql
@@ -1,78 +1,78 @@
 INSERT INTO posts (item, title, location, description)
-VALUES ('1', 'Groot Figure Toy', '1 Fusionopolis Walk,
+VALUES (1, 'Groot Figure Toy', '1 Fusionopolis Walk,
  #11-04 South Tower, Solaris, Singapore 138628',
  'Marvel Guardians of the Galaxy Dancing Groot Bobble
  Figure Toy Doll Funko POP');
 INSERT INTO posts (item, title, location, description)
-VALUES ('2', 'Bicycle', '1922 Heliport Loop, Bloomington,
+VALUES (2, 'Bicycle', '1922 Heliport Loop, Bloomington,
  Indiana, US', 'Classic Moulton Mini Deluxe Bicycle');
 INSERT INTO posts (item, title, location, description)
-VALUES ('3', 'Mountain Bike', '1486 Wildrose Lane, LANESBOROUGH,
+VALUES (3, 'Mountain Bike', '1486 Wildrose Lane, LANESBOROUGH,
  Massachusetts, US', 'Ancheer 14"Mini Electric Power Bicycle
  26" Foldable Electric Mountain Bike');
 INSERT INTO posts (item, title, location, description)
-VALUES ('4', 'Book on the road', '171 tras street 04-171a
+VALUES (4, 'Book on the road', '171 tras street 04-171a
  union building, 079025, Singapore', 'JACK KEROUAC 1st edition
  of his MOST FAMOUS book ON THE ROAD');
 INSERT INTO posts (item, title, location, description)
-VALUES ('5', 'The Anatomy Coloring Book', '1 Fusionopolis Walk,
+VALUES (5, 'The Anatomy Coloring Book', '1 Fusionopolis Walk,
  #11-04 South Tower, Solaris, Singapore 138628', 'The Anatomy
  Coloring Book 4th Edition by Wynn Kapit (English) Paperback Book');
 INSERT INTO posts (item, title, location, description)
-VALUES ('6', 'Adaptor', '20 Benoi Crescent Jurong, Singapore 629983',
+VALUES (6, 'Adaptor', '20 Benoi Crescent Jurong, Singapore 629983',
  'Macbook adaptor in good condition');
 INSERT INTO posts (item, title, location, description)
-VALUES ('7', 'Hand Druck', '6b boon tiong road 10-55, 165006,
+VALUES (7, 'Hand Druck', '6b boon tiong road 10-55, 165006,
  Singapore', 'hmk111ua4 aluminum hand truck, horizontal
  loop handle, 18" x 7-1/2" aluminum');
 INSERT INTO posts (item, title, location, description)
-VALUES ('8', 'Play Station 4', '108 Gerald Drive #04-38, 799035,
+VALUES (8, 'Play Station 4', '108 Gerald Drive #04-38, 799035,
  Singapore', 'Sony PlayStation 4 PS4 500GB Original
  Matte Black CUH-1215A ONE CONSOLE');
 INSERT INTO posts (item, title, location, description)
-VALUES ('9', 'Xbox One', '60 Admiralty Road West #02-01 PROJECT
+VALUES (9, 'Xbox One', '60 Admiralty Road West #02-01 PROJECT
  OFFICE NO 8, 759947, Singapore', 'MICROSOFT XBOX ONE S 500GB
  BATTLEFIELD 1 CONSOLE BUNDLE ZQ9-0028 - WHITE');
 INSERT INTO posts (item, title, location, description)
-VALUES ('10', 'Step Ladder', '48, Hillview Ter #04-01, Hillview
+VALUES (10, 'Step Ladder', '48, Hillview Ter #04-01, Hillview
  Bldg Singapore 669269', 'Louisville Ladder AS3002 300-Pound Duty
  Rating Aluminum Stepladder 2-Foot');
 INSERT INTO posts (item, title, location, description)
-VALUES ('11','VR box','1 Fusionopolis Walk, #11-04 South Tower, Solaris,
+VALUES (11,'VR box','1 Fusionopolis Walk, #11-04 South Tower, Solaris,
  Singapore 138628','Vr Box Virtual Reality Movies Games 3d Glasses
  For 4.7-6.0 Smart Phone');
 INSERT INTO posts (item, title, location, description)
-VALUES ('12','Funko Pop','1922 Heliport Loop, Bloomington,
+VALUES (12,'Funko Pop','1922 Heliport Loop, Bloomington,
  Indiana, US','Funko Pop Disney Alice In Wonderland: Mad Hatter Vinyl
  Collectible Action Figure');
 INSERT INTO posts (item, title, location, description)
-VALUES ('13','Step Ladder','1486 Wildrose Lane, LANESBOROUGH, Massachusetts,
+VALUES (13,'Step Ladder','1486 Wildrose Lane, LANESBOROUGH, Massachusetts,
  US','Folding Aluminium Ladder 3 Non Slip Ribbed Steps 3.3kg');
 INSERT INTO posts (item, title, location, description)
-VALUES ('14','Sketch Box','171 tras street 04-171a union building, 079025,
+VALUES (14,'Sketch Box','171 tras street 04-171a union building, 079025,
  Singapore','French Easel Wooden Sketch Box Portable Folding Durable
  Artist Painters Tripod');
 INSERT INTO posts (item, title, location, description)
-VALUES ('15','Tool Box','1 Fusionopolis Walk, #11-04 South Tower, Solaris,
+VALUES (15,'Tool Box','1 Fusionopolis Walk, #11-04 South Tower, Solaris,
  Singapore 138628','3 PC TOOL BOX INSIDE DRAWER ORGANIZER DIVIDER FOR
  KITCHEN DESK TOOL ROLLAWAY');
 INSERT INTO posts (item, title, location, description)
-VALUES ('16','Computer Repair Tool','20 Benoi Crescent Jurong,
+VALUES (16,'Computer Repair Tool','20 Benoi Crescent Jurong,
  Singapore 629983','Computer Repair Tool Kit Precision
  For Laptop Electronics PC Repair 76 in 1 Set');
 INSERT INTO posts (item, title, location, description)
-VALUES ('17','LEGO','6b boon tiong road 10-55, 165006,
+VALUES (17,'LEGO','6b boon tiong road 10-55, 165006,
  Singapore','LEGO Elsa Magical Ice Palace Frozen Disney Princess
  Set 41148');
 INSERT INTO posts (item, title, location, description)
-VALUES ('18','Book','108 Gerald Drive #04-38, 799035,
+VALUES (18,'Book','108 Gerald Drive #04-38, 799035,
  Singapore','Star Wars Art: Concept by Lucasfilm Ltd
  (English) Hardcover Book');
 INSERT INTO posts (item, title, location, description)
-VALUES ('19','Textbook','60 Admiralty Road West #02-01 PROJECT
+VALUES (19,'Textbook','60 Admiralty Road West #02-01 PROJECT
  OFFICE NO 8, 759947, Singapore','LINUX ALL-IN-ONE FOR DUMMIES
  - DULANEY, EMMETT - NEW PAPERBACK BOOK');
 INSERT INTO posts (item, title, location, description)
-VALUES ('20','Screwdriver','48, Hillview Ter #04-01, Hillview
+VALUES (20,'Screwdriver','48, Hillview Ter #04-01, Hillview
  Bldg Singapore 669269','Makita XSF03Z 18V Lithium-Ion Brushless
  Cordless Drywall Screwdriver, Bare Tool');
