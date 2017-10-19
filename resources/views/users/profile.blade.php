3229170b13c4ebaa6882d6d9ef2090416dc7da3a
DongShaocong <qq423785549@gmail.com>
2017-10-19 19:41:32 +0800

    add description field to the post display in profile page

--- resources/views/users/profile.blade.php
+++ resources/views/users/profile.blade.php
@@ -96,7 +96,7 @@
 
                         <?php
 
-                            $sql = "select p.title, p.location, p.created_at, p.postid, i.name from posts p, items i where p.item = i.itemid AND i.owner = '".
+                            $sql = "select p.title, p.location, p.created_at, p.postid, p.description, i.name from posts p, items i where p.item = i.itemid AND i.owner = '".
 							$email."';";
                             $posts = $db->query($sql);
                             $index = 1;
@@ -106,6 +106,7 @@
                                 echo "Item name:". $row['name']; echo"<br />";
                                 echo "Post title:" .$row['title'];echo"<br />";
                                 echo "Created at:" .$row['created_at'];echo"<br />";
+                                echo "Description:" .$row['description'];echo"<br />";
                                 echo "Pick up location:". $row['location'];echo"<br /><br /><br />";
 
                                 $index++;
