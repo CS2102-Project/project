0c0e271f96d45a58151cb2564989b34ce4410629
wangzexin <wang.zexin@dhs.sg>
2017-10-21 23:24:41 +0800

    Add query to get average popularity of user's items owned

--- resources/views/users/statistics.sql
+++ resources/views/users/statistics.sql
@@ -1,8 +1,13 @@
 CREATE VIEW item_popularity AS
-SELECT i.itemid as itemid, COUNT(*) AS popularity
+SELECT i.itemid as itemid, i.owner as owner, COUNT(*) AS popularity
 FROM items i, posts p, bids b
 WHERE i.itemid = p.item AND p.postid = b.post
-GROUP BY i.itemid;
+GROUP BY i.itemid, i.owner;
 
 SELECT itemid, AVERAGE(popularity)
 FROM item_popularity;
+
+SELECT u.email, AVERAGE(i.popularity)
+FROM users u, item_popularity item
+WHERE u.email = i.owner
+GROUP BY u.email;
