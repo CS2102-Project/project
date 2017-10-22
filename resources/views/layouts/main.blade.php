4f94bcd9dfa92710d4ebbc06b9beec2104865dec
DongShaocong <qq423785549@gmail.com>
2017-10-22 23:40:42 +0800

    Remove links in app name for logged-in users

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -47,7 +47,7 @@
                 </button>
 
                 <!-- Branding Image -->
-                <a class="navbar-brand" href="{{ url('/') }}">
+                <a class="navbar-brand" >
                     {{ config('app.name', 'Laravel') }}
                 </a>
             </div>
